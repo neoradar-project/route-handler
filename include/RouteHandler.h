adbc2b53fe71c80ce1a3e237cb089cca8de7fe0e
George Barlow <george@barlw.uk>
2024-11-28 22:53:12 +0000

    Fix

--- include/RouteHandler.h
+++ include/RouteHandler.h
@@ -3,57 +3,45 @@
 #include "Navdata.h"
 #include "Parser.h"
 #include "types/Procedure.h"
-#include <unordered_map>
 #include <memory>
 #include <string>
-#include <types/Waypoint.h>
 #include <thread>
+#include <types/Waypoint.h>
+#include <unordered_map>
 using namespace RouteParser;
 
-class RouteHandler
-{
+class RouteHandler {
 public:
-  RouteHandler()
-  {
-    this->navdata = std::make_shared<RouteParser::NavdataObject>();
-    this->Parser = std::make_shared<RouteParser::ParserHandler>(this->navdata);
-  }
-  ~RouteHandler()
-  {
-    this->Parser.reset();
-    this->navdata.reset();
-  }
-
-  std::shared_ptr<ParserHandler> GetParser();
-  void Bootstrap(ILogger logFunc,
-                 std::string waypointsFile,
-                 std::unordered_multimap<std::string, Procedure> procedures,
-                 std::string airwaysFile, std::string isecFile = "")
-  {
-    Log::SetLogger(logFunc);
-    navdata->SetProcedures(procedures);
-
-    navdata->LoadAirwayNetwork(airwaysFile);
-    navdata->LoadWaypoints(waypointsFile);
-
-    std::thread isecThread;
-    if (!isecFile.empty())
+    RouteHandler()
     {
-      isecThread = std::thread([this, &isecFile]()
-                               { navdata->LoadIntersectionWaypoints(isecFile); });
+        this->navdata = std::make_shared<RouteParser::NavdataObject>();
+        this->Parser = std::make_shared<RouteParser::ParserHandler>(this->navdata);
+    }
+    ~RouteHandler()
+    {
+        this->Parser.reset();
+        this->navdata.reset();
     }
 
-    if (!isecFile.empty() && isecThread.joinable())
+    std::shared_ptr<ParserHandler> GetParser();
+    void Bootstrap(ILogger logFunc, std::string navdataDbFile,
+        std::unordered_multimap<std::string, Procedure> procedures,
+        std::string airwaysDbFile)
     {
-      isecThread.join();
+        Log::SetLogger(logFunc);
+        navdata->SetProcedures(procedures);
+
+        navdata->LoadAirwayNetwork(airwaysDbFile);
+        navdata->LoadWaypoints(navdataDbFile);
+
+        Log::info("RouteHandler is ready.");
+        this->isReady = true;
     }
 
-    Log::info("RouteHandler is ready.");
-    this->isReady = true;
-  }
+    bool IsReady() { return this->isReady; }
 
 private:
-  std::shared_ptr<RouteParser::ParserHandler> Parser = nullptr;
-  std::shared_ptr<RouteParser::NavdataObject> navdata = nullptr;
-  bool isReady = false;
+    std::shared_ptr<RouteParser::ParserHandler> Parser = nullptr;
+    std::shared_ptr<RouteParser::NavdataObject> navdata = nullptr;
+    bool isReady = false;
 };
