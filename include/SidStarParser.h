7ac1a576a720bc902496a5910608a58f4d5eec2b
Pierre Ferran <pierr3@users.noreply.github.com>
2025-02-24 19:51:17 +0100

    Fix issues

--- include/SidStarParser.h
+++ include/SidStarParser.h
@@ -36,68 +36,69 @@ public:
     std::optional<std::string> runway = FindRunway(token);
     const std::vector<std::string> parts = absl::StrSplit(token, '/');
     if (parts.empty() || parts.size() < 2) {
-      // Safety check, should never happen
-      return FoundProcedure {
-        {}, runway,
-            {ParsingError{
-                ParsingErrorType::INVALID_DATA,
-                fmt::format("Token split failed despite finding runway: {} {}",
-                            token, icao),
-                tokenIndex, token, ParsingErrorLevel::PARSE_ERROR}};
-      }
-      std::string procedureToken =
-          runway ? parts[0] : token; // If we have a runway, we know that the
-                                     // string is split in at least 2 parts
 
-      if (runway && procedureToken.length() == 4 && procedureToken == icao) {
-        return FoundProcedure{procedureToken, runway, std::nullopt,
-                              std::nullopt};
-      }
-
-      const auto &procedures = NavdataObject::GetProcedures();
-      std::vector<RouteParser::Procedure> matchingProcedures;
-
-      for (auto it = procedures.equal_range(procedureToken);
-           it.first != it.second; ++it.first) {
-        if (it.first->second.icao == icao && it.first->second.type == type) {
-          matchingProcedures.push_back(it.first->second);
-        }
-      }
-
-      if (matchingProcedures.empty()) {
-        // No procedure found for the given token, but runway might have been
-        // found
-        return FoundProcedure{std::nullopt, runway, std::nullopt};
-      }
-
-      // There is no runway but matching procedures
-      if (!runway) {
-        return FoundProcedure{procedureToken, runway, matchingProcedures[0]};
-      }
-
-      if (runway) {
-        // There is a runway, there are matching procedure, let's try and catch
-        // the right one
-        for (const auto &procedure : matchingProcedures) {
-          if (procedure.runway == runway) {
-            // Full, valid match for procedure + runway
-            return FoundProcedure{procedureToken, runway, procedure};
-          }
-        }
-      }
-
-      // No matching runway found for procedure, return the runway with an error
       return FoundProcedure{
-          procedureToken,
+          {},
           runway,
-          matchingProcedures[0],
+          {},
           {ParsingError{
-              ParsingErrorType::PROCEDURE_RUNWAY_MISMATCH,
-              fmt::format("No matching runway {} found for procedure {} at {}, "
-                          "returning first matching procedure",
-                          runway.value_or("N/A"), procedureToken, icao),
-              tokenIndex, procedureToken, ParsingErrorLevel::PARSE_ERROR}}};
+              ParsingErrorType::INVALID_DATA,
+              fmt::format("Token split failed despite finding runway: {} {}",
+                          token, icao),
+              tokenIndex, token, ParsingErrorLevel::PARSE_ERROR}}};
     }
-  };
 
-} // namespace RouteParser
+    std::string procedureToken =
+        runway ? parts[0] : token; // If we have a runway, we know that the
+                                   // string is split in at least 2 parts
+
+    if (runway && procedureToken.length() == 4 && procedureToken == icao) {
+      return FoundProcedure{procedureToken, runway, std::nullopt};
+    }
+
+    const auto &procedures = NavdataObject::GetProcedures();
+    std::vector<RouteParser::Procedure> matchingProcedures;
+
+    for (auto it = procedures.equal_range(procedureToken);
+         it.first != it.second; ++it.first) {
+      if (it.first->second.icao == icao && it.first->second.type == type) {
+        matchingProcedures.push_back(it.first->second);
+      }
+    }
+
+    if (matchingProcedures.empty()) {
+      // No procedure found for the given token, but runway might have been
+      // found
+      return FoundProcedure{std::nullopt, runway, std::nullopt};
+    }
+
+    // There is no runway but matching procedures
+    if (!runway) {
+      return FoundProcedure{procedureToken, runway, matchingProcedures[0]};
+    }
+
+    if (runway) {
+      // There is a runway, there are matching procedure, let's try and catch
+      // the right one
+      for (const auto &procedure : matchingProcedures) {
+        if (procedure.runway == runway) {
+          // Full, valid match for procedure + runway
+          return FoundProcedure{procedureToken, runway, procedure};
+        }
+      }
+    }
+
+    // No matching runway found for procedure, return the runway with an error
+    return FoundProcedure{
+        procedureToken,
+        runway,
+        matchingProcedures[0],
+        {ParsingError{
+            ParsingErrorType::PROCEDURE_RUNWAY_MISMATCH,
+            fmt::format("No matching runway {} found for procedure {} at {}, "
+                        "returning first matching procedure",
+                        runway.value_or("N/A"), procedureToken, icao),
+            tokenIndex, procedureToken, ParsingErrorLevel::PARSE_ERROR}}};
+  }
+};
+}; // namespace RouteParser
